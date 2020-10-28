028a5ed86925d5590f70f955097c9d161667a8a0
Anna Ponyavina <ponyavina1997@mail.ru>
2020-10-28 16:07:15 +0300

    create: lesson02

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 'use strict';
 
-let money,
-    income,
-    addExpenses,
-    deposit,
-    mission,
-    period;
+const money = 150000,
+    income = 'фриланс',
+    addExpenses = 'Интернет, Вода, Газ, Отопление, Электричество',
+    deposit = true,
+    mission = 10000000,
+    period = 10,
+    budgetDay = money / 30;
 
-alert('Привет!');
-console.log('Еще раз привет :)');
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+console.log(addExpenses.toLowerCase().split(', '));
+console.log(budgetDay);
