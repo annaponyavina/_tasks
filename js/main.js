3a9af34b5d9330e6278e1d3af3bab62f345535c4
Anna Ponyavina <ponyavina1997@mail.ru>
2020-10-28 16:28:45 +0300

    create: lesson03_hard

--- js/main.js
+++ js/main.js
@@ -1,37 +1,39 @@
 'use strict';
 
-const money = +prompt('Ваш месячный доход?');
-const income = 'фриланс';
-const addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-const deposit = confirm('Есть ли у вас депозит в банке?');
-const mission = 10000000;
-const expensesNumber = 2;
-const expenses1 = prompt('Введите обязательную статью расходов?');
-const amount1 = +prompt('Во сколько это обойдется?');
-const expenses2 = prompt('Введите обязательную статью расходов?');
-const amount2 = +prompt('Во сколько это обойдется?');
-const budgetMonth = money - (amount1 + amount2);
-const period = Math.ceil(mission / budgetMonth);
-const budgetDay = Math.floor(budgetMonth / 30);
+const lang = 'ru';
 
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
-console.log(addExpenses.length);
-console.log('Период равен ' + period + ' месяцев');
-console.log('Цель заработать ' + mission + ' рублей');
-console.log(addExpenses.toLowerCase().split(', '));
-console.log('Бюджет на месяц: ' + budgetMonth);
-console.log('Бюджет на день: ' + budgetDay);
+let days = new Map();
 
-if (budgetDay >= 1200) {
-    console.log('У вас высокий уровень дохода');
-} else if (budgetDay >= 600 && budgetDay < 1200) {
-    console.log('У вас средний уровень дохода');
-} else if (budgetDay > 0 && budgetDay < 600) {
-    console.log('К сожалению у вас уровень дохода ниже среднего');
-} else if (budgetDay === 0) {
-    console.log('Ваш доход дырка от бублика 0_0');
+days.set('ru', 'Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресение');
+days.set('en', 'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+
+// Через if
+if (lang === 'ru') {
+    console.log(days.get(lang));
+} else if (lang === 'en') {
+    console.log(days.get(lang));
 } else {
-    console.log('Что-то пошло не так');
+    console.log('Указан неверный язык');
 }
+
+// Через switch case
+switch (lang) {
+    case 'ru':
+        console.log(days.get(lang));
+        break;
+    case 'en':
+        console.log(days.get(lang));
+        break;
+    default:
+        console.log('Указан неверный язык');
+        break;
+}
+
+// Через многомерный массив
+console.log(days.get(lang));
+
+
+// Вторая задача
+const namePerson = 'Артем';
+const result = namePerson === 'Артем' ? 'директор' : namePerson === 'Максим' ? 'преподаватель' : 'студент';
+console.log(result);
