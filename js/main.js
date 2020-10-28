8f219d40bd40ac9ed8c7f9bf3e3a46aebfd1b2e1
Anna Ponyavina <ponyavina1997@mail.ru>
2020-10-28 16:43:24 +0300

    create: lesson04

--- js/main.js
+++ js/main.js
@@ -1,39 +1,46 @@
 'use strict';
 
-const lang = 'ru';
+const money = +prompt('Ваш месячный доход?');
+const income = 'фриланс';
+const addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+const deposit = confirm('Есть ли у вас депозит в банке?');
+const mission = 10000000;
+const expensesNumber = 2;
+const expenses1 = prompt('Введите обязательную статью расходов?');
+const amount1 = +prompt('Во сколько это обойдется?');
+const expenses2 = prompt('Введите обязательную статью расходов?');
+const amount2 = +prompt('Во сколько это обойдется?');
 
-let days = new Map();
+const getExpensesMonth = (a, b) => a + b;
+const getAccumulatedMonth = (a, b) => a - b;
+const getTargetMonth = (a, b) => Math.ceil(a / b);
+const showTypeOf = data => console.log(data, typeof (data));
 
-days.set('ru', 'Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресение');
-days.set('en', 'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+showTypeOf(money);
+showTypeOf(income);
+showTypeOf(deposit);
 
-// Через if
-if (lang === 'ru') {
-    console.log(days.get(lang));
-} else if (lang === 'en') {
-    console.log(days.get(lang));
-} else {
-    console.log('Указан неверный язык');
-}
+const accumulatedMonth = getAccumulatedMonth(money, getExpensesMonth(amount1, amount2));
+const budgetDay = Math.floor(accumulatedMonth / 30);
+const period = getTargetMonth(mission, accumulatedMonth);
 
-// Через switch case
-switch (lang) {
-    case 'ru':
-        console.log(days.get(lang));
-        break;
-    case 'en':
-        console.log(days.get(lang));
-        break;
-    default:
-        console.log('Указан неверный язык');
-        break;
-}
+console.log('Обязательные расходы: ', getExpensesMonth(amount1, amount2));
+console.log(addExpenses.toLowerCase().split(', '));
+console.log('Период равен ' + period + ' месяцев');
+console.log('Бюджет на день: ' + budgetDay);
 
-// Через многомерный массив
-console.log(days.get(lang));
+const getStatusIncome = () => {
+    if (budgetDay >= 1200) {
+        return ('У вас высокий уровень дохода');
+    } else if (budgetDay >= 600 && budgetDay < 1200) {
+        return ('У вас средний уровень дохода');
+    } else if (budgetDay > 0 && budgetDay < 600) {
+        return ('К сожалению у вас уровень дохода ниже среднего');
+    } else if (budgetDay === 0) {
+        return ('Ваш доход дырка от бублика 0_0');
+    } else {
+        return ('Что-то пошло не так');
+    }
+};
 
-
-// Вторая задача
-const namePerson = 'Артем';
-const result = namePerson === 'Артем' ? 'директор' : namePerson === 'Максим' ? 'преподаватель' : 'студент';
-console.log(result);
+console.log(getStatusIncome());
